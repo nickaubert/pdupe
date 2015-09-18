eb9c008b718dec517e041b1d65b0bbc5e76872d8
nickaubert@gmail.com <nickaubert@gmail.com>
2015-09-18 13:34:43 -0400

    dividing photo into cells, getting color data

--- pdupe.go
+++ pdupe.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	// "os"
+)
+
 import "github.com/gographics/imagick/imagick"
 
 /*
@@ -8,6 +13,11 @@ import "github.com/gographics/imagick/imagick"
  */
 
 func main() {
+
+	// argsWithoutProg := os.Args[1:]
+
+	hSlices := uint(32)
+	vSlices := uint(32)
 
 	imagick.Initialize()
 	defer imagick.Terminate()
@@ -20,8 +30,51 @@ func main() {
 		fmt.Println("Error: ", err)
 	}
 
-	length, err := mw.GetImageLength()
+	cellWidth := Divide(float64(mw.GetImageWidth()), hSlices)
+	cellHeight := Divide(float64(mw.GetImageHeight()), vSlices)
 
-	fmt.Println("Hello, world: ", length)
+	// fmt.Println("Image cols: ", cellWidth)
+	// fmt.Println("Image rows: ", cellHeight)
 
+	/* will need to make sure we don't overflow at boundaries since we're rounding */
+	for vCell := uint(0); vCell < vSlices; vCell++ {
+		ch := cellHeight
+		if vCell+1 == vSlices {
+			ch = mw.GetImageHeight() - (vCell * vSlices)
+		}
+		for hCell := uint(0); hCell < hSlices; hCell++ {
+			fmt.Printf("cells: %d, %d : ", vCell, hCell)
+			cw := cellWidth
+			if hCell+1 == hSlices {
+				cw = mw.GetImageWidth() - (hCell * hSlices)
+			}
+			mc := mw.GetImageRegion(cw, ch, int(vCell*vSlices), int(hCell*hSlices))
+			redAvg, _, err := mc.GetImageChannelMean(imagick.CHANNEL_RED)
+			grnAvg, _, err := mc.GetImageChannelMean(imagick.CHANNEL_GREEN)
+			bluAvg, _, err := mc.GetImageChannelMean(imagick.CHANNEL_BLUE)
+			if err != nil {
+				fmt.Println("channel error: , ", err)
+			}
+			// these channels are 16 bit? knock down to 8 bit colors
+			redRnd := Divide(redAvg, 256)
+			grnRnd := Divide(grnAvg, 256)
+			bluRnd := Divide(bluAvg, 256)
+			fmt.Println(redRnd, grnRnd, bluRnd)
+		}
+		// os.Exit(0) // TESTING
+	}
+
+}
+
+/* from https://gist.github.com/DavidVaini/10308388 */
+func Divide(num float64, denom uint) (newVal uint) {
+	var rounded float64
+	prod := num / float64(denom)
+	mod := math.Mod(prod, 1)
+	if mod >= 0.5 {
+		rounded = math.Ceil(prod)
+	} else {
+		rounded = math.Floor(prod)
+	}
+	return uint(rounded)
 }
