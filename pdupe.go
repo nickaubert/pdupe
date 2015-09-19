58b24c80a23bc22898789a188a525ca58ea0a16b
nickaubert@gmail.com <nickaubert@gmail.com>
2015-09-18 22:32:56 -0400

    moved stuff into function, made filename argument

--- pdupe.go
+++ pdupe.go
@@ -3,10 +3,17 @@ package main
 import (
 	"fmt"
 	"math"
-	// "os"
+	"os"
 )
 
 import "github.com/gographics/imagick/imagick"
+
+const (
+	HSlices = 32
+	VSlices = 32
+	HReSize = 128
+	VReSize = 128
+)
 
 /*
  *  https://gowalker.org/github.com/gographics/imagick/imagick
@@ -30,78 +37,13 @@ import "github.com/gographics/imagick/imagick"
 
 func main() {
 
-	// argsWithoutProg := os.Args[1:]
-
-	hreSize := uint(128)
-	vreSize := uint(128)
-
-	hSlices := uint(32)
-	vSlices := uint(32)
-
 	imagick.Initialize()
 	defer imagick.Terminate()
 
-	mw := imagick.NewMagickWand()
-	defer mw.Destroy()
-
-	err := mw.ReadImage("IMG_0696.jpg")
-	if err != nil {
-		fmt.Println("Error: ", err)
-	}
-
-	err = mw.ResizeImage(hreSize, vreSize, imagick.FILTER_GAUSSIAN, 0.25)
-	if err != nil {
-		fmt.Println("resize error: ", err)
-	}
-
-	cellWidth := hreSize / hSlices
-	cellHeight := vreSize / vSlices
-
-	fmt.Println("width: ", mw.GetImageWidth())
-	fmt.Println("height: ", mw.GetImageHeight())
-
-	/*
-			 * colorData will hold color info for the image
-			 * since we're using a fixed number of cells,
-		     * this will be a one dimensional array of 1024 * 3 members
-	*/
-	colorData := make([]uint8, 3*hSlices*hSlices)
-	index := 0
-
-	mc := imagick.NewMagickWand()
-
-	for vCell := uint(0); vCell < vSlices; vCell++ {
-
-		for hCell := uint(0); hCell < hSlices; hCell++ {
-
-			mc = mw.GetImageRegion(cellWidth, cellHeight, int(vCell*cellHeight), int(hCell*cellWidth))
-			var redAvg, grnAvg, bluAvg float64
-
-			// fmt.Println("cell: ", vCell, hCell, int(hCell*hSlices))
-			if redAvg, _, err = mc.GetImageChannelMean(imagick.CHANNEL_RED); err != nil {
-				fmt.Println("red channel error: , ", err)
-			}
-			if grnAvg, _, err = mc.GetImageChannelMean(imagick.CHANNEL_GREEN); err != nil {
-				fmt.Println("green channel error: , ", err)
-			}
-			if bluAvg, _, err = mc.GetImageChannelMean(imagick.CHANNEL_BLUE); err != nil {
-				fmt.Println("blue channel error: , ", err)
-			}
-
-			// these channels are 16 bit? knock down to 8 bit colors
-			redRnd := uint8(Divide(redAvg, 256))
-			grnRnd := uint8(Divide(grnAvg, 256))
-			bluRnd := uint8(Divide(bluAvg, 256))
-
-			// colorData = append(colorData, redRnd, grnRnd, bluRnd)
-			colorData[index+0] = redRnd
-			colorData[index+1] = grnRnd
-			colorData[index+2] = bluRnd
-			index += 3
-			fmt.Println(redRnd, grnRnd, bluRnd)
-
-		}
-		// os.Exit(0) // TESTING
+	for _, arg := range os.Args[1:] {
+		fmt.Println("arg: ", arg)
+		colorData := getColorData(arg)
+		fmt.Println(arg, "data: ", colorData)
 	}
 
 }
@@ -117,4 +59,91 @@ func Divide(num float64, denom uint) (newVal uint) {
 		rounded = math.Floor(prod)
 	}
 	return uint(rounded)
+}
+
+/* not entirely sure color channels work this way, but don't see anything more reliable*/
+func make8bit(fullColor float64, depth uint) uint8 {
+	var flatColor uint8
+	o := float64(depth - 8)
+	m := math.Pow(2, o)
+	flatColor = uint8(fullColor / m)
+	return flatColor
+}
+
+func difference(a, b uint) uint {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func getColorData(file string) []uint8 {
+
+	mw := imagick.NewMagickWand()
+	defer mw.Destroy()
+
+	colorData := make([]uint8, 3*HSlices*VSlices)
+
+	err := mw.ReadImage(file)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return colorData
+
+	}
+
+	err = mw.ResizeImage(HReSize, VReSize, imagick.FILTER_GAUSSIAN, 0.25)
+	if err != nil {
+		fmt.Println("resize error: ", err)
+	}
+
+	cellWidth := HReSize / HSlices
+	cellHeight := VReSize / VSlices
+
+	fmt.Println("width: ", mw.GetImageWidth())
+	fmt.Println("height: ", mw.GetImageHeight())
+
+	/*
+			 * colorData will hold color info for the image
+			 * since we're using a fixed number of cells,
+		     * this will be a one dimensional array of 1024 * 3 members
+	*/
+	index := 0
+
+	redDepth := mw.GetImageChannelDepth(imagick.CHANNEL_RED)
+	grnDepth := mw.GetImageChannelDepth(imagick.CHANNEL_GREEN)
+	bluDepth := mw.GetImageChannelDepth(imagick.CHANNEL_BLUE)
+
+	mc := imagick.NewMagickWand()
+
+	for vCell := 0; vCell < VSlices; vCell++ {
+
+		for hCell := 0; hCell < HSlices; hCell++ {
+
+			mc = mw.GetImageRegion(uint(cellWidth), uint(cellHeight), int(vCell*cellHeight), int(hCell*cellWidth))
+			var redAvg, grnAvg, bluAvg float64
+
+			if redAvg, _, err = mc.GetImageChannelMean(imagick.CHANNEL_RED); err != nil {
+				fmt.Println("red channel error: , ", err)
+			}
+			if grnAvg, _, err = mc.GetImageChannelMean(imagick.CHANNEL_GREEN); err != nil {
+				fmt.Println("green channel error: , ", err)
+			}
+			if bluAvg, _, err = mc.GetImageChannelMean(imagick.CHANNEL_BLUE); err != nil {
+				fmt.Println("blue channel error: , ", err)
+			}
+
+			// make sure we're using 8 bit color
+			redRnd := make8bit(redAvg, redDepth)
+			grnRnd := make8bit(grnAvg, grnDepth)
+			bluRnd := make8bit(bluAvg, bluDepth)
+
+			colorData[index+0] = redRnd
+			colorData[index+1] = grnRnd
+			colorData[index+2] = bluRnd
+			index += 3
+
+		}
+	}
+
+	return colorData
 }
